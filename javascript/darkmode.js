3484859571f89b56bdd875d87b198dd61a3d22b8
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-02-13 17:00:22 -0600

    working on design area

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,1 +1,18 @@
 let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', 'enabled')
+}
+
+const diableDarkMode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkMode', null)
+}
+
+darkModeToggle.addEventListener('click', () => {
+    console.log('test')
+});
